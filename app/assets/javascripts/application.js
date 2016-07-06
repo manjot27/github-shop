8ce06153707d21b343ce0695fd4d98eee1c19bd4
DadiHall <dadih32@gmail.com>
2016-07-06 02:18:06 +0000

    added Offcanvas loggin sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,26 @@ $(document).ready(function () {
 				    $(this).toggleClass("active");
 			  });
 		});
+
+
+
+$(function() {
+  $('.toggle-nav').click(function() {
+    // Calling a function in case you want to expand upon this.
+    toggleNav();
+  });
+});
+
+
+
+function toggleNav() {
+if ($('#site-wrapper').hasClass('show-nav')) {
+  // Do things on Nav Close
+  $('#site-wrapper').removeClass('show-nav');
+} else {
+  // Do things on Nav Open
+  $('#site-wrapper').addClass('show-nav');
+}
+
+//$('#site-wrapper').toggleClass('show-nav');
+}
